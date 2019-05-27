7a2ffa951e9e84c410ec9494ad49bbc33a57c714
岡田俊樹 <>
2019-05-27 12:21:27 +0900

    DB内のメッセージを表示できるように

--- docker_clearMng/helloworld/urls.py
+++ docker_clearMng/helloworld/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.send_words, name='hello world'),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='hello world'),
 ]
--- docker_clearMng/helloworld/views.py
+++ docker_clearMng/helloworld/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Message
 
 # Create your views here.
 def helloworld(request):
@@ -8,3 +9,9 @@ def helloworld(request):
 def send_words(request):
     word_list = ["cat", "dog", "cow"]
     return render(request, 'receive_words.html', {'list': word_list})
+
+def index(request):
+    d = {
+        'messages': Message.objects.all(),
+    }
+    return render(request, 'index.html', d)
